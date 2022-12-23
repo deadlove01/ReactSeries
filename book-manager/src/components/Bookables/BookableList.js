f97dcff30cebc1854ed7bc6105b0f0e635f8d867
Ravi Le <ravi.le@soxes.ch>
2022-12-23 13:14:38 +0700

    Update BookableList.js

--- book-manager/src/components/Bookables/BookableList.js
+++ book-manager/src/components/Bookables/BookableList.js
@@ -1,4 +1,5 @@
 import React, {useState} from 'react'
+import {FaArrowRight} from 'react-icons/fa'
 import data from '../../Data/static.json'
 
 const BookableList = () => {
@@ -6,7 +7,9 @@ const BookableList = () => {
     const [bookableIndex, setBookableIndex] = useState(1);
     const {bookables} = data;
 
-    const group = "Rooms";
+    const [group, setGroup] = useState("Kit");
+    const groups = [...new Set(bookables.map(b => b.group))];
+
     const bookablesInGroup = bookables.filter(b => b.group === group);
 
     const changeBookable = (selectedIndex) => {
@@ -14,22 +17,44 @@ const BookableList = () => {
         console.log(selectedIndex);
     }
 
+
+    const nextBookable = () =>{
+        setBookableIndex(i => (i+1) % bookablesInGroup.length);
+    };
+
     return (
-        <ul className='bookables items-list-nav'>
-            {
-            bookablesInGroup.map((b, i) => (
-                <li key={
-                        b.title
-                    }
-                    className={
-                        i === bookableIndex ? "selected" : ""
-                }>
-                    <button className='btn' onClick={() => changeBookable(i)}>{
-                        b.title
-                    }</button>
-                </li>
-            ))
-        } </ul>
+        <>
+            <select value={group} onChange={(e) => setGroup(e.target.value)}>
+                {groups.map(g => <option value={g} key={g} > {g} </option>)}
+            </select>
+
+            <ul className='bookables items-list-nav'>
+                {
+                bookablesInGroup.map((b, i) => (
+                    <li key={
+                            b.title
+                        }
+                        className={
+                            i === bookableIndex ? "selected" : ""
+                    }>
+                        <button className='btn'
+                            onClick={
+                                () => changeBookable(i)
+                        }>
+                            {
+                            b.title
+                        }</button>
+                    </li>
+                ))
+            } </ul>
+            <p>
+                <button className='btn' autoFocus 
+                    onClick={nextBookable}>
+                    <FaArrowRight/>
+                    <span>Next</span>
+                </button>
+            </p>
+        </>
     );
 }
 
