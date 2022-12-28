e2431029689e08d3f255e67d7f5de89a47191534
Ravi Le <ravi.le@soxes.ch>
2022-12-29 01:37:51 +0700

    add routes

--- myecommerce/src/App.js
+++ myecommerce/src/App.js
@@ -1,11 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
-import { Home } from './pages/Home';
+import logo from "./logo.svg";
+import "./App.css";
+import { Home } from "./pages/Home";
+import { Route, Routes, Switch } from "react-router-dom";
+
+const HatsPage = () => (
+  <div>
+    <h1>HATS PAGE </h1>
+  </div>
+);
 
 function App() {
   return (
-    <div className="App">
-      <Home />
+    <div>
+      <Routes>
+        <Route exact path="/" element={<Home />} />
+        <Route path="/hats" element={<HatsPage />} />
+      </Routes>
     </div>
   );
 }
