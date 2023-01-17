4dabbeb5785cca661fae8910e5c0da498bf18038
Ravi Le <ravi.le@soxes.ch>
2023-01-18 02:14:39 +0700

    Update App.js

--- ecom-course/src/App.js
+++ ecom-course/src/App.js
@@ -1,22 +1,42 @@
-import { Route, Routes } from 'react-router-dom';
-import './App.css';
-import Header from './components/header/Header';
-import Auth from './pages/auth/auth';
-import { Home } from './pages/homepage/home';
-import Shop from './pages/shop/Shop';
+import { useEffect, useState } from "react";
+import { Route, Routes } from "react-router-dom";
+import "./App.css";
+import Header from "./components/header/Header";
+import { auth } from "./firebase/FirebaseUtils";
+import Auth from "./pages/auth/auth";
+import { Home } from "./pages/homepage/home";
+import Shop from "./pages/shop/Shop";
 
-function App() {
+const App = () => {
+
+  const [currentUser, setCurrentUser] = useState(null)
+
+  useEffect(() => {
+    const unsub = auth.onAuthStateChanged(async userAuth => {
+      if (userAuth)
+      {
+        setCurrentUser(userAuth);
+        console.log(userAuth);
+      }
+    });
+  
+    return async () => {
+      unsub();
+    }
+  }, [])
+  
+
+
   return (
     <div>
       <Header />
       <Routes>
-        <Route exact path='/' element={<Home />} />
-        <Route path='/shop' element={<Shop />} />
-        <Route path='/signin' element={<Auth />} />
+        <Route exact path="/" element={<Home />} />
+        <Route path="/shop" element={<Shop />} />
+        <Route path="/signin" element={<Auth />} />
       </Routes>
-
     </div>
   );
-}
+};
 
 export default App;
